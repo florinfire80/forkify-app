417f7fe376c7ec8ba288344b212a92f1b8886460
florinfire80 <florinfire80@yahoo.com>
2024-11-17 15:06:10 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6dc19b6b-0870-424c-85b4-b0dbb740e47a';
+export const MODAL_CLOSE_SEC = 2.5;
